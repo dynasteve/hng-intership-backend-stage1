b7b8b351b0af349dc7dec2a792fe39698bd238e0
Stephen Akande <tosisiyesteve@gmail.com>
2025-10-21 02:19:27 +0100

    added Procfile

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+from unittest.mock import Base
 from fastapi import FastAPI
 from .routes import strings
 from .database import engine
@@ -6,17 +8,18 @@ from . import models
 # Ensure tables are created (for dev/demo). In prod use migrations (alembic).
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="HNG Internship Backend Stage 1")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup code
+    Base.metadata.create_all(bind=engine)
+    yield
+    # Shutdown code (optional)
+    # e.g., closing resources
+
+app = FastAPI(title="HNG Internship Backend Stage 1", lifespan=lifespan)
 
 app.include_router(strings.router)
 
 @app.get("/")
 def root():
     return {"message": "Welcome to String Analyzer Service!"}
-
-@app.on_event("startup")
-def init_db():
-    from app.database import Base, engine
-    import app.models
-    Base.metadata.create_all(bind=engine)
-    
